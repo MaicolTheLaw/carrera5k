6d15805c5732ded09e476a3cc47431ff48531f9d
MaicolTheLaw <miguelalatorre1@gmail.com>
2015-11-07 05:27:39 -0800

    CRUD Runners

--- app/controllers/runners_controller.rb
+++ app/controllers/runners_controller.rb
@@ -1,10 +1,7 @@
 class RunnersController < ApplicationController
 	before_filter :authenticate_user!
 
-
 	def show 
-		@run = Runner.where(user_id:  current_user.id).take
-		@runner = Runner.find(@run[:id])
 	end	
 
 	def new
@@ -19,17 +16,16 @@ class RunnersController < ApplicationController
 	end	
 
 	def edit
-		@run = Runner.where(user_id:  current_user.id).take
-		@runner = Runner.find(@run[:id])
+		@runner = Runner.where(user_id:  current_user.id).take
 	end	
 	
-
 	def update
+		@runner = Runner.where(user_id:  current_user.id).take
 		@runner.update_attributes(runner_params)	
+		redirect_to root_path
 	end	
 
 	private
-
   	def runner_params
     	params.require(:runner).permit(:full_name, :birthday, :gender, :address, :city_of_birth, :state, :runners_team, :home_number, :mobile_number, :illness_exists, :illness_description)
   	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-
   resources :runners
+  post '/runners/:id', to: 'runners#update'
   resources :race_editions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
