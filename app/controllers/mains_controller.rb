7d6ccc8e1b7d45e9b78a1af6a5d81a83c209c652
MaicolTheLaw <miguelalatorre1@gmail.com>
2015-11-12 16:33:47 -0800

    Final

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -24,12 +24,20 @@ class MainsController < ApplicationController
 		@runner_id = rid.id
 		race_e = RaceEdition.order(created_at: :desc).last
 		@race_edition_id = race_e.id
+		@race_name = race_e.race_name
+		@race_date = race_e.race_date
+		@race_address = race_e.race_address
 		@has_payment = true
+		@price = 100
 		if RaceRunner.where(has_kit: true).count > 500
 		@has_kit = false
 		else
 		@has_kit = true
 		end	
+
+		user = User.find(current_user.id)
+		@user = user.email
+		UserMailer.breakfast_report(@user).deliver_now
 
 
 	end	
